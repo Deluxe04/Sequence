a498b2ac9a0a3b8f7ace654951d8626d51ec58a2
Deluxe04 <akmalov.1967@mail.ru>
2026-04-01 02:08:17 +0300

    sequence.h

--- sequence.h
+++ sequence.h
@@ -28,11 +28,47 @@ public:
     //Базовый комплект операций map-reduce
     virtual Sequence<T>* Map(T (*f)(const T&)) const = 0;
     virtual Sequence<T>* Where(bool (*predicate)(const T&)) const = 0;
-    virtual T Reduce(T (*func)(T, const T&), T initial) const = 0;
-    virtual Option<T> GetFirst(bool (*predicate)(const T&)) const = 0;
-    virtual Option<T> GetLast (bool (*predicate)(const T&)) const = 0;
  
     virtual IEnumerator<T>* GetEnumerator() const override = 0;
+    
+    Option<T> FindElement(bool (*predicate)(const T&), bool fromFirst) const
+    {
+        int len = GetLength();
+        int start = fromFirst ? 0 : len - 1;
+        int end   = fromFirst ? len : -1;
+        int step  = fromFirst ? 1 : -1;
+        for (int i = start; i != end; i += step)
+        {
+            const T& item = Get(i);
+            if (!predicate || predicate(item))
+            {
+                return Option<T>(item);
+            }
+        }
+        return Option<T>();
+    }
+
+    virtual T Reduce(T (*func)(T, const T&), T initial) const
+    {
+        T result = initial;
+        int len = GetLength();
+        for (int i = 0; i < len; ++i)
+        {
+            result = func(result, Get(i));
+        }
+        return result;
+    }
+
+    virtual Option<T> GetFirst(bool (*predicate)(const T&)) const
+    {
+        return FindElement(predicate, true);
+    }
+
+    virtual Option<T> GetLast(bool (*predicate)(const T&)) const
+    {
+        return FindElement(predicate, false);
+    }
+    
 };
 
 #endif // SEQUENCE_H
