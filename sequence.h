35dabc8769469fabe68c45d1d79b3f1ed8addd8d
Deluxe04 <akmalov.1967@mail.ru>
2026-03-31 11:15:19 +0300

    sequence.h

--- sequence.h
+++ sequence.h
@@ -2,25 +2,37 @@
 #define SEQUENCE_H
 
 #include "exceptions.h"
+#include "option.h"
+#include "iterator.h"
 
 template <class T>
-class Sequence {
+class Sequence : public IEnumerable<T>
+{
 public:
     virtual ~Sequence() {}
 
-    // Декомпозиция
+    //Декомпозиция
     virtual const T& GetFirst() const = 0;
-    virtual const T& GetLast() const = 0;
+    virtual const T& GetLast()  const = 0;
     virtual const T& Get(int index) const = 0;
     virtual Sequence<T>* GetSubsequence(int startIndex, int endIndex) const = 0;
     virtual int GetLength() const = 0;
 
-    // Операции
+    //Операции
     virtual Sequence<T>* Append(const T& item) = 0;
     virtual Sequence<T>* Prepend(const T& item) = 0;
     virtual Sequence<T>* InsertAt(const T& item, int index) = 0;
     virtual Sequence<T>* RemoveAt(int index) = 0;
     virtual Sequence<T>* Concat(Sequence<T>* other) = 0;
+
+    //Базовый комплект операций map-reduce
+    virtual Sequence<T>* Map(T (*f)(const T&)) const = 0;
+    virtual Sequence<T>* Where(bool (*predicate)(const T&)) const = 0;
+    virtual T Reduce(T (*func)(T, const T&), T initial) const = 0;
+    virtual Option<T> GetFirst(bool (*predicate)(const T&)) const = 0;
+    virtual Option<T> GetLast (bool (*predicate)(const T&)) const = 0;
+ 
+    virtual IEnumerator<T>* GetEnumerator() const override = 0;
 };
 
 #endif // SEQUENCE_H
